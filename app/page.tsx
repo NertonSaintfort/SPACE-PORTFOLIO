d1e7be4093dbc9d4eff83f543a53fa04be8f6d5f
NertonSaintfort <SaintfortNerton@gmail.com>
2023-12-24 16:38:23 -0500

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Skills from "@/components/main/Skills";
 import Hero from "@/components/main/Hero";
 
 export default function Home() {
   return(
     <main className="h-full w-full">
-<div className="flex flex-col h-[850px] gap-20">
+<div className="flex flex-col  gap-20">
 <Hero />
+<Skills />
 </div>
   </main>
   ) ;
